407e64c6f269553d7330344b82810ce8b7b13680
Carl Pearson <pearson@illinois.edu>
2018-03-12 17:48:52 -0500

    initial commit

---
+++ common/common.hpp
@@ -0,0 +1,32 @@
+#ifndef COMMON_HPP
+#define COMMON_HPP
+
+#include <cuda_runtime_api.h>
+#include <cuda.h>
+#include <iostream>
+
+#define RT_CHECK(ans)                                                   \
+  { rtAssert((ans), __FILE__, __LINE__); }
+inline void rtAssert(cudaError_t code, const char *file, int line, bool abort = true) {
+  if (code != cudaSuccess) {
+    std::cerr << "CUDA_CHECK: " << cudaGetErrorString(code) << " " << file << " "
+        << line << std::endl;
+    if (abort)
+      exit(code);
+  }
+}
+
+#define DR_CHECK(ans)                                                   \
+  { drAssert((ans), __FILE__, __LINE__); }
+inline void drAssert(CUresult code, const char *file, int line, bool abort = true) {
+  if (code != CUDA_SUCCESS) {
+    const char *str;
+    cuGetErrorString(code, &str);
+    std::cerr << "DR_CHECK: " << str << " " << file << " "
+        << line << std::endl;
+    if (abort)
+      exit(code);
+  }
+}
+
+#endif
