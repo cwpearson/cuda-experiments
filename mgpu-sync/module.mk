639cb2c18ea7645b3afc08381373fa5234bf8166
Carl Pearson <carl.w.pearson@gmail.com>
2018-03-28 20:31:04 -0500

    remove gencode from modules

--- mgpu-sync/module.mk
+++ mgpu-sync/module.mk
@@ -3,7 +3,7 @@ MODULE := mgpu-sync
 TARGETS += $(MODULE)/main $(MODULE)/main.ptx
 
 $(MODULE)/main: mgpu-sync/main.cu
-	$(NVCC) $(NVCCFLAGS) $^ -std=c++11 -Xcompiler -Wall,-Wextra,-O3 -gencode $(GENCODE) -rdc=true -o $@ -lnvToolsExt
+	$(NVCC) $(NVCCFLAGS) $^ -std=c++11 -Xcompiler -Wall,-Wextra,-O3 -rdc=true -o $@ -lnvToolsExt
 
 $(MODULE)/main.ptx: mgpu-sync/main.cu
-	$(NVCC) $(NVCCFLAGS) $^ -std=c++11 -Xcompiler -Wall,-Wextra,-O3 -gencode $(GENCODE) -rdc=true -ptx -src-in-ptx -o $@ -lnvToolsExt
+	$(NVCC) $(NVCCFLAGS) $^ -std=c++11 -Xcompiler -Wall,-Wextra,-O3 -rdc=true -ptx -src-in-ptx -o $@ -lnvToolsExt
