d984dd78500d66170c59968ca58d687f5b205a02
Carl Pearson <pearson@illinois.edu>
2018-04-03 17:00:14 -0500

    use new common in prefetch-bw

--- prefetch-bw/main.cu
+++ prefetch-bw/main.cu
@@ -12,24 +12,24 @@
 
 #include "common/common.hpp"
 
-static void prefetch_bw(const int dstDev, const int srcDev, const size_t count)
+static void prefetch_bw(const Device &dstDev, const Device &srcDev, const size_t count)
 {
 
-  if (srcDev != cudaCpuDeviceId)
+  if (srcDev.is_gpu())
   {
-    RT_CHECK(cudaSetDevice(srcDev));
+    RT_CHECK(cudaSetDevice(srcDev.cuda_device_id()));
     RT_CHECK(cudaFree(0));
   }
 
-  if (dstDev != cudaCpuDeviceId)
+  if (dstDev.is_gpu())
   {
-    RT_CHECK(cudaSetDevice(dstDev));
+    RT_CHECK(cudaSetDevice(dstDev.cuda_device_id()));
     RT_CHECK(cudaFree(0));
   }
 
-  if (srcDev != cudaCpuDeviceId)
+  if (srcDev.is_gpu())
   {
-    RT_CHECK(cudaSetDevice(srcDev));
+    RT_CHECK(cudaSetDevice(srcDev.cuda_device_id()));
   }
 
   void *ptr;
@@ -42,14 +42,14 @@ static void prefetch_bw(const int dstDev, const int srcDev, const size_t count)
   {
     // Try to get allocation on source
     nvtxRangePush("move to src");
-    RT_CHECK(cudaMemPrefetchAsync(ptr, count, srcDev));
+    RT_CHECK(cudaMemPrefetchAsync(ptr, count, srcDev.cuda_device_id()));
     RT_CHECK(cudaDeviceSynchronize());
     nvtxRangePop();
 
     // Prefetch to device and time.
     nvtxRangePush("tx");
     auto start = std::chrono::high_resolution_clock::now();
-    RT_CHECK(cudaMemPrefetchAsync(ptr, count, dstDev));
+    RT_CHECK(cudaMemPrefetchAsync(ptr, count, dstDev.cuda_device_id()));
     RT_CHECK(cudaDeviceSynchronize());
     auto end = std::chrono::high_resolution_clock::now();
     nvtxRangePop();
@@ -73,32 +73,52 @@ int main(void)
   RT_CHECK(cudaGetDeviceCount(&numDevs));
 
   std::vector<int> devIds;
-  for (int dev = 0; dev < numDevs; ++dev)
+  auto cpus = get_cpus();
+  auto gpus = get_gpus();
+  auto devs = cpus;
+  for (const auto g : gpus)
   {
-    devIds.push_back(dev);
+    devs.push_back(g);
   }
-  devIds.push_back(cudaCpuDeviceId);
+
+  size_t freeMem = -1ul;
+  for (auto d : devs)
+  {
+    if (d.is_gpu())
+    {
+      size_t fr, to;
+      RT_CHECK(cudaMemGetInfo(&fr, &to));
+
+      if (fr < freeMem)
+      {
+        freeMem = fr;
+      }
+    }
+  }
 
   // print header
   printf("Transfer Size (MB),");
-  for (const auto src : devIds)
+  for (const auto src : devs)
   {
-    for (const auto dst : devIds)
+    for (const auto dst : devs)
     {
       if (src != dst)
       {
-        printf("%d:%d,", src, dst);
+        printf("%s to %s (prefetch),", src.name().c_str(), dst.name().c_str());
       }
     }
   }
   printf("\n");
 
-  for (size_t count = 2048; count <= 4 * 1024ul * 1024ul * 1024ul; count *= 2)
+  auto counts = Sequence::geometric(2048, freeMem, 2) |
+                Sequence::geometric(2048 * 1.5, freeMem, 2);
+
+  for (auto count : counts)
   {
     printf("%f", count / 1024.0 / 1024.0);
-    for (const auto src : devIds)
+    for (const auto src : devs)
     {
-      for (const auto dst : devIds)
+      for (const auto dst : devs)
       {
         if (src != dst)
         {
