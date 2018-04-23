7f80b26d2d7d761bc45bb86407f32e2775c1e616
Carl Pearson <pearson@illinois.edu>
2018-04-23 18:14:47 -0500

    try to fix caching in cpu-cpu-df

--- cpu-cpu-df/main.cpp
+++ cpu-cpu-df/main.cpp
@@ -1,92 +1,105 @@
-#include <cstdio>
-#include <cassert>
-#include <cstdlib>
-#include <sstream>
-#include <chrono>
-#include <vector>
 #include <algorithm>
-#include <numeric>
-#include <cstring>
+#include <cassert>
+#include <chrono>
 #include <climits>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <numeric>
+#include <sstream>
+#include <vector>
 
-#include <unistd.h>
-#include <omp.h>
 #include <numa.h>
+#include <omp.h>
+#include <unistd.h>
 
 #include "common/common.hpp"
 #include "op.hpp"
 
-static void prefetch_bw(const Device &dstDev, const Device &srcDev, const size_t count, const size_t stride, const size_t nCpus)
-{
+static void omp_bind(const Device &d) {
+  bind_cpu(d);
+#pragma omp parallel
+  { bind_cpu(d); }
+}
+
+#ifdef OP_WR
+static void wr_bw(const Device &dstDev, const Device &srcDev,
+                  const size_t count, const size_t stride, const size_t nCpus) {
 
   assert(srcDev.is_cpu() && dstDev.is_cpu());
+  omp_set_num_threads(nCpus);
   const long pageSize = sysconf(_SC_PAGESIZE);
 
   // create source allocation
-#ifdef OP_RD
-  //printf("\nalloc on %s\n", srcDev.name().c_str());
-  bind_cpu(srcDev);
-#elif OP_WR
   bind_cpu(dstDev);
-#else
-#error "woah"
-#endif
   double *ptr = static_cast<double *>(aligned_alloc(pageSize, count));
   std::memset(ptr, 0, count);
 
-#ifdef OP_RD
-  bind_cpu(dstDev);
-#elif OP_WR
-  bind_cpu(srcDev);
-#else
-#error "woah"
-#endif
-
-  omp_set_num_threads(nCpus);
-
-#pragma omp parallel
-  {
-#ifdef OP_RD
-    //printf("\nrd on %s\n", dstDev.name().c_str());
-    bind_cpu(dstDev);
-#elif OP_WR
-    bind_cpu(srcDev);
-#else
-#error "woah"
-#endif
-  }
+  omp_bind(srcDev);
 
   double dummy;
   const size_t numIters = 15;
-  for (size_t i = 0; i < numIters; ++i)
-  {
-    // Access from Device and Time
-    //#pragma omp parallel
-    {
-      //#pragma omp barrier
-      auto start = std::chrono::high_resolution_clock::now();
-#ifdef OP_RD
-      cpu_read_8(&dummy, ptr, count, stride);
-#elif OP_WR
-      cpu_write_8(&dummy, ptr, count, stride);
-#else
-#error "woah"
-#endif
+  for (size_t i = 0; i < numIters; ++i) {
 
-      //#pragma omp barrier
-      //    if (omp_get_thread_num() == 0) {
-      auto end = std::chrono::high_resolution_clock::now();
-      std::chrono::duration<double> txSeconds = end - start;
-      printf("%lu,%s,%s,%lu,%.2f\n", count, srcDev.name().c_str(), dstDev.name().c_str(), nCpus, count / txSeconds.count());
-      //    }
-    }
+    // invalidate allocation in src caches
+    omp_bind(dstDev);
+    std::memset(ptr, 0, count);
+
+    // Access from Device and Time
+    omp_bind(srcDev);
+
+    auto start = std::chrono::high_resolution_clock::now();
+    cpu_write_8(&dummy, ptr, count, stride);
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> txSeconds = end - start;
+    printf("%lu,%s,%s,%lu,%.2f\n", count, srcDev.name().c_str(),
+           dstDev.name().c_str(), nCpus, count / txSeconds.count());
   }
 
   free(ptr);
 }
+#endif
 
-int main(void)
-{
+#ifdef OP_RD
+static void rd_bw(const Device &dstDev, const Device &srcDev,
+                  const size_t count, const size_t stride, const size_t nCpus) {
+
+  assert(srcDev.is_cpu() && dstDev.is_cpu());
+  const long pageSize = sysconf(_SC_PAGESIZE);
+  omp_set_num_threads(nCpus);
+
+  // create source allocation
+  omp_bind(srcDev);
+  double *ptr = static_cast<double *>(aligned_alloc(pageSize, count));
+  std::memset(ptr, 0, count);
+
+  omp_bind(dstDev);
+
+  double dummy;
+  const size_t numIters = 15;
+  for (size_t i = 0; i < numIters; ++i) {
+
+    // invalidate data in dst cache
+    omp_bind(srcDev);
+    std::memset(ptr, 0, count);
+
+    // Access from Device and Time
+    omp_bind(dstDev);
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    cpu_read_8(&dummy, ptr, count, stride);
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> txSeconds = end - start;
+    printf("%lu,%s,%s,%lu,%.2f\n", count, srcDev.name().c_str(),
+           dstDev.name().c_str(), nCpus, count / txSeconds.count());
+  }
+  free(ptr);
+}
+#endif
+
+int main(void) {
   numa_set_strict(1);
   numa_exit_on_error = 1;
   auto cpus = get_cpus();
@@ -103,17 +116,18 @@ int main(void)
   auto counts = Sequence::geometric(2048, freeMem, 2) |
                 Sequence::geometric(2048 * 1.5, freeMem, 2);
 
-  for (auto count : counts)
-  {
-    for (size_t par = 1; par <= numCpusPerNode; par *= 2)
-    {
-      for (const auto src : cpus)
-      {
-        for (const auto dst : cpus)
-        {
-          if (src != dst)
-          {
-            prefetch_bw(dst, src, count, 8, par);
+  for (auto count : counts) {
+    for (size_t par = 1; par <= numCpusPerNode; par *= 2) {
+      for (const auto src : cpus) {
+        for (const auto dst : cpus) {
+          if (src != dst) {
+#ifdef OP_RD
+            rd_bw(dst, src, count, 8, par);
+#elif OP_WR
+            wr_bw(dst, src, count, 8, par);
+#else
+#error "woah"
+#endif
           }
         }
       }
